33cfcd3c875b6c257fd5f6516f674c0bf0486cb5
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-09 11:24:28 -0800

    update UI

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
-import { haikuSyllableCount } from "./js/haiku";
+import { Haiku } from "./js/haiku";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './css/styles.css';
 document.addEventListener("submit", (e) => {
     e.preventDefault();
-    const input = document.querySelector("#input").value;
-    const haikuP = document.createElement("p");
+    const input = new Haiku(document.querySelector("#input").value);
+    const haikuDiv = document.createElement("div");
     const syllableP = document.createElement("p");
     const mainDiv = document.createElement("div");
     const haikus = document.getElementById("haikus");
+    const line1 = document.createElement("p");
+    const line2 = document.createElement("p");
+    const line3 = document.createElement("p");
     haikus.innerHTML = "";
-    if (input.trim() === "") {
-        haikuP.append(`Do you hear it the\n Silence in the text box here\n For there is nothing`);
-        mainDiv.append(haikuP);
+    if (document.querySelector("#input").value.trim() === "") {
+        line1.append("Do you hear it the");
+        line2.append("Silence in the text box here");
+        line3.append("For there is nothing");
+        haikuDiv.append(line1, line2, line3);
+        mainDiv.append(haikuDiv);
         haikus.append(mainDiv);
     } else {
-        const syllables = haikuSyllableCount(input);
+        const syllables = input.syllables();
         const syllableString = `Syllables: ${syllables}`;
         if (syllables !== 17) {
-            haikuP.append(`Haiku it is not\n
-        For your syllables were off\n
-        Try again my friend`);
+            line1.append("Haiku it is not");
+            line2.append("For your syllables were off");
+            line3.append("Try again my friend");
+            haikuDiv.append(line1, line2, line3);
             syllableP.append(`You wrote ${syllables} syllables`);
-            mainDiv.append(haikuP, syllableP);
+            mainDiv.append(haikuDiv, syllableP);
             haikus.append(mainDiv);
         } else {
-            haikuP.append(input);
+            const lineArray = input.lineSplit();
+            line1.append(lineArray[0]);
+            line2.append(lineArray[1]);
+            line3.append(lineArray[2]);
+            haikuDiv.append(line1, line2, line3);
             syllableP.append(syllableString);
-            mainDiv.append(haikuP, syllableP);
+            mainDiv.append(haikuDiv, syllableP);
             haikus.append(mainDiv);
         }
     }
--- src/js/haiku.js
+++ src/js/haiku.js
@@ -1,52 +1,61 @@
-export { silentTrim, syllableCount, haikuSplit, haikuSyllableCount };
+export { Haiku };
 
-const noPlurals = (word) => {
-    if (word.charAt(word.length - 1).match(/[s]/i)) {
-        return word.slice(0, word.length - 1);
+class Haiku {
+    constructor(haikuString) {
+        this.haikuString = haikuString;
     }
-    return word;
-};
 
-const silentTrim = word => {
-
-    let trimmed = word;
-    const vowelLength = word.match(/[aeiouy]/gi);
-    if (vowelLength.length === 1) {
-        return word.replace(/[aeiouy]/gi, "a");
-    } else if (word.charAt(word.length - 2).match(/[t]/i) && word.charAt(word.length - 1).match(/[e]/i)) {
-        trimmed = word.slice(0, word.length - 1);
-    } else if (word.charAt(word.length - 2).match(/[tl]/i) && word.charAt(word.length - 1).match(/[aeiouy]/i)) {
-        trimmed = word.slice(0, word.length - 1) + "a";
-    } else if (word.charAt(word.length - 1).match(/[aeiou]/i)) {
-        trimmed = word.slice(0, word.length - 1);
+    syllables() {
+        const noPlurals = (word) => {
+            if (word.charAt(word.length - 1).match(/[s]/i)) {
+                return word.slice(0, word.length - 1);
+            }
+            return word;
+        };
+        const silentTrim = word => {
+            let trimmed = word;
+            const vowelLength = word.match(/[aeiouy]/gi);
+            if (vowelLength.length === 1) {
+                return word.replace(/[aeiouy]/gi, "a");
+            } else if (word.charAt(word.length - 2).match(/[t]/i) && word.charAt(word.length - 1).match(/[e]/i)) {
+                trimmed = word.slice(0, word.length - 1);
+            } else if (word.charAt(word.length - 2).match(/[tl]/i) && word.charAt(word.length - 1).match(/[aeiouy]/i)) {
+                trimmed = word.slice(0, word.length - 1) + "a";
+            } else if (word.charAt(word.length - 1).match(/[aeiou]/i)) {
+                trimmed = word.slice(0, word.length - 1);
+            }
+            return trimmed.replace(/(eau)/gi, "a").replace(/[aeiou]{2}/gi, "a");
+        };
+        const syllableCount = (word) => {
+            let count = 0;
+            const letterArray = silentTrim(noPlurals(word)).split("");
+            letterArray.forEach((letter, i) => {
+                if (letter.match(/[aeiou]/i)) {
+                    count++;
+                } else if (letter === "y" && i !== 0 && i !== letterArray.length - 1) {
+                    count++;
+                }
+            });
+            return count;
+        };
+        const haikuSplit = (haiku) => {
+            let haikuArray = haiku.split(" ");
+            return haikuArray;
+        };
+        const haikuSyllableCount = (haiku) => {
+            if (haiku.match(/[aeiouy]/gi)) {
+                let totalSyllables = 0;
+                haikuSplit(haiku).forEach(word => {
+                    totalSyllables = totalSyllables + syllableCount(word);
+                });
+                return totalSyllables;
+            } return 0;
+        };
+        return haikuSyllableCount(this.haikuString);
     }
-    return trimmed.replace(/(eau)/gi, "a").replace(/[aeiou]{2}/gi, "a");
-};
 
-const syllableCount = (word) => {
-    let count = 0;
-    const letterArray = silentTrim(noPlurals(word)).split("");
-    letterArray.forEach((letter, i) => {
-        if (letter.match(/[aeiou]/i)) {
-            count++;
-        } else if (letter === "y" && i !== 0 && i !== letterArray.length - 1) {
-            count++;
-        }
-    });
-    return count;
-};
-
-const haikuSplit = (haiku) => {
-    let haikuArray = haiku.split(" ");
-    return haikuArray;
-};
-
-const haikuSyllableCount = (haiku) => {
-    if (haiku.match(/[aeiouy]/gi)) {
-        let totalSyllables = 0;
-        haikuSplit(haiku).forEach(word => {
-            totalSyllables = totalSyllables + syllableCount(word);
-        });
-        return totalSyllables;
-    } return 0;
-};
+    lineSplit() {
+        const lineArray = this.haikuString.split(/\r?\n/);
+        return lineArray;
+    }
+}
